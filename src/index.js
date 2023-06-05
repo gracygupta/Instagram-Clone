39f621b77ea5ee444b8cab80170b1fcc36f8afc4
gracygupta <hzgracy@gmail.com>
2023-06-06 01:20:23 +0530

    firebase setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import FirebaseContext from './context/firebase'
+import { firebase, FieldValue } from './lib/firebase'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
+
+React.render(
+  <FirebaseContext.Provider value={{ firebase, FieldValue }} />,
+  <App />, document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
